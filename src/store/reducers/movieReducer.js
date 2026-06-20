7a93ec5b1cccae94d165f12ba8d47764bde09546
Selen Akın <127742376+selenakinn@users.noreply.github.com>
2026-06-20 18:13:34 +0300

    Implement movie reducer with add and delete functionality

---
+++ src/store/reducers/movieReducer.js
@@ -0,0 +1,23 @@
+import { ADD_MOVIE,DELETE_MOVIE } from '../actions/movieActions.js';
+import movies from '../../data.js';
+
+const initialState = {
+  movies: movies,
+  appTitle: 'IMDB Movie Database',
+};
+
+const reducer = (state=initialState, action) => {
+  switch (action.type) {
+    case DELETE_MOVIE:
+      return {...state,
+        movies: state.movies.filter(movie => movie.id !== action.payload)
+      };
+    case ADD_MOVIE:
+      return {...state,
+             movies: [...state.movies,action.payload]}
+    default:
+      return state;
+  }
+};
+
+export default reducer;
